27a80ff0dc8086d27f1537d3980db2bbea41303f
jonahthakid <Jonah@proshop.inc>
2025-12-26 13:18:46 -0500

    Remove playwright, add railway config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverComponentsExternalPackages: ['cheerio', 'undici', 'playwright'],
-  },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals.push('cheerio', 'undici');
-    }
-    return config;
+    serverComponentsExternalPackages: ['cheerio', 'undici'],
   },
 }
 
